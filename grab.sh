d115860e7ccbe0a851f155b769b3f8d3995befac
blackrockjet <menadue@gmail.com>
2021-08-13 06:19:59 +0100

    need to work within permissions when grabbing

--- grab.sh
+++ grab.sh
@@ -1,3 +1,6 @@
-cp -R /tree/downloaded_tools/pico/picoputer1 picoputer
-cp -R /tree/downloaded_tools/pico/picoputer1-eclipse picoputer-eclipse
+sudo cp -R /tree/downloaded_tools/pico/picoputer1 picoputer
+sudo cp -R /tree/downloaded_tools/pico/picoputer1-eclipse picoputer-eclipse
+sudo chmod -R a+rw picoputer
+sudo chmod -R a+rw picoputer-eclipse
+
 
